de0ff1da11617f9c1e5fab6e7ac35d9d5e979635
Kevin Chen <kevinmichaelchen@gmail.com>
2022-10-26 12:30:25 -0400

    Fix gRPC server EXTRACTION

--- pkg/temporal/interceptors.go
+++ pkg/temporal/interceptors.go
@@ -7,27 +7,11 @@ import (
 )
 
 func ClientInterceptors() ([]interceptor.ClientInterceptor, error) {
-	i, err := opentelemetry.NewTracingInterceptor(opentelemetry.TracerOptions{
-		//TextMapPropagator: otel.GetTextMapPropagator(),
-	})
+	i, err := opentelemetry.NewTracingInterceptor(opentelemetry.TracerOptions{})
 	if err != nil {
 		return nil, fmt.Errorf("failed to create OTEL tracing interceptor: %w", err)
 	}
 	return []interceptor.ClientInterceptor{
 		i,
 	}, nil
-	//return nil, nil
-}
-
-func WorkerInterceptors() ([]interceptor.WorkerInterceptor, error) {
-	//i, err := opentelemetry.NewTracingInterceptor(opentelemetry.TracerOptions{
-	//	TextMapPropagator: otel.GetTextMapPropagator(),
-	//})
-	//if err != nil {
-	//	return nil, fmt.Errorf("failed to create OTEL tracing interceptor: %w", err)
-	//}
-	//return []interceptor.WorkerInterceptor{
-	//	i,
-	//}, nil
-	return nil, nil
 }
