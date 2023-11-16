d96cdb7829665e62d6e7b2e4730f41924c0d493c
Kevin Chen <kevinmichaelchen@gmail.com>
2023-11-15 19:56:19 -0500

    feat: WithoutServerPeerAttributes for low-cardinality telemetry

--- pkg/connect/interceptors.go
+++ pkg/connect/interceptors.go
@@ -13,6 +13,9 @@ func Interceptors() []connect.Interceptor {
 			// Trust the client's tracing information. With this option, servers
 			// will create child spans for each request.
 			otelconnect.WithTrustRemote(),
+			// Reduce metrics and tracing cardinality. Drop high-cardinality
+			// attributes like the server IP address and the remote port number.
+			otelconnect.WithoutServerPeerAttributes(),
 		),
 	}
 }
