71c99bcc824dccc7bf6680596eff751754d9f2be
Kevin Chen <kevinmichaelchen@gmail.com>
2023-11-16 18:11:36 -0500

    feat: Use Connect clients instead of gRPC clients (#55)

--- cmd/saga/worker/app/worker/worker.go
+++ cmd/saga/worker/app/worker/worker.go
@@ -4,15 +4,18 @@ package worker
 import (
 	"context"
 	"fmt"
+	"net/http"
 
+	"buf.build/gen/go/kevinmichaelchen/licenseapis/connectrpc/go/license/v1beta1/licensev1beta1connect"
+	"buf.build/gen/go/kevinmichaelchen/orgapis/connectrpc/go/org/v1beta1/orgv1beta1connect"
+	"buf.build/gen/go/kevinmichaelchen/profileapis/connectrpc/go/profile/v1beta1/profilev1beta1connect"
+	"connectrpc.com/connect"
 	"github.com/sirupsen/logrus"
-	"go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc"
 	"go.temporal.io/sdk/client"
 	"go.temporal.io/sdk/worker"
 	"go.uber.org/fx"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials/insecure"
 
+	pkgConnect "github.com/kevinmichaelchen/temporal-saga-grpc/pkg/connect"
 	"github.com/kevinmichaelchen/temporal-saga-grpc/pkg/saga"
 )
 
@@ -25,27 +28,11 @@ const (
 // Module - An FX module for a Temporal worker.
 var Module = fx.Module("worker",
 	fx.Provide(
-		fx.Annotate(
-			NewController,
-			fx.ParamTags(
-				`name:"license"`,
-				`name:"org"`,
-				`name:"profile"`,
-			),
-		),
+		NewController,
 		NewWorker,
-		fx.Annotate(
-			NewConnToLicense,
-			fx.ResultTags(`name:"license"`),
-		),
-		fx.Annotate(
-			NewConnToOrg,
-			fx.ResultTags(`name:"org"`),
-		),
-		fx.Annotate(
-			NewConnToProfile,
-			fx.ResultTags(`name:"profile"`),
-		),
+		NewLicenseClient,
+		NewOrgClient,
+		NewProfileClient,
 	),
 	fx.Invoke(
 		RegisterWorkflowAndActivities,
@@ -53,45 +40,53 @@ var Module = fx.Module("worker",
 )
 
 // NewController - Returns a new controller for our Temporal workflow.
-func NewController(license, org, profile *grpc.ClientConn) *saga.Controller {
-	return saga.NewController(license, org, profile)
+func NewController(
+	licenseClient licensev1beta1connect.LicenseServiceClient,
+	orgClient orgv1beta1connect.OrgServiceClient,
+	profileClient profilev1beta1connect.ProfileServiceClient,
+) *saga.Controller {
+	return saga.NewController(licenseClient, orgClient, profileClient)
 }
 
-// NewConnToLicense - Returns a new gRPC connection to the License service.
-func NewConnToLicense() (*grpc.ClientConn, error) {
+// NewLicenseClient - Returns a new Connect client for the License service.
+func NewLicenseClient() licensev1beta1connect.LicenseServiceClient {
 	addr := fmt.Sprintf("localhost:%d", servicePortLicense)
 
-	return dial(addr)
+	return licensev1beta1connect.NewLicenseServiceClient(
+		http.DefaultClient,
+		addr,
+		connect.WithInterceptors(pkgConnect.Interceptors()...),
+	)
 }
 
-// NewConnToOrg - Returns a new gRPC connection to the Org service.
-func NewConnToOrg() (*grpc.ClientConn, error) {
+// NewOrgClient - Returns a new Connect client for the Org service.
+func NewOrgClient() orgv1beta1connect.OrgServiceClient {
 	addr := fmt.Sprintf("localhost:%d", servicePortOrg)
 
-	return dial(addr)
+	return orgv1beta1connect.NewOrgServiceClient(
+		http.DefaultClient,
+		addr,
+		connect.WithInterceptors(pkgConnect.Interceptors()...),
+	)
 }
 
-// NewConnToProfile - Returns a new gRPC connection to the Profile service.
-func NewConnToProfile() (*grpc.ClientConn, error) {
+// NewProfileClient - Returns a new Connect client for the Profile service.
+func NewProfileClient() profilev1beta1connect.ProfileServiceClient {
 	addr := fmt.Sprintf("localhost:%d", servicePortProfile)
 
-	return dial(addr)
-}
-
-func dial(addr string) (*grpc.ClientConn, error) {
-	conn, err := grpc.Dial(addr,
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
-		grpc.WithUnaryInterceptor(otelgrpc.UnaryClientInterceptor()),
+	return profilev1beta1connect.NewProfileServiceClient(
+		http.DefaultClient,
+		addr,
+		connect.WithInterceptors(pkgConnect.Interceptors()...),
 	)
-	if err != nil {
-		return nil, fmt.Errorf("unable to dial gRPC connection: %w", err)
-	}
-
-	return conn, nil
 }
 
 // NewWorker - Returns a new worker for our Temporal workflow.
-func NewWorker(lifecycle fx.Lifecycle, c client.Client, ctrl *saga.Controller) (*worker.Worker, error) {
+func NewWorker(
+	lifecycle fx.Lifecycle,
+	c client.Client,
+	ctrl *saga.Controller,
+) (*worker.Worker, error) {
 	// This worker hosts both Workflow and Activity functions
 	temporalWorker := worker.New(c, saga.CreateLicenseTaskQueue, worker.Options{
 		// worker.Start() only return errors on start, so we need to catch
