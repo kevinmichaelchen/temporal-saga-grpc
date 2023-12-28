cb858e9ecbe26fd98bf445d7516e1ecea5357b3b
Kevin Chen <kevinmichaelchen@gmail.com>
2023-12-27 22:29:16 -0500

    fix: Fix duplicate app dir

--- ui/src/app/layout.tsx
+++ ui/src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en">
       <body
         className={cn(
           'bg-background min-h-screen font-sans antialiased',
