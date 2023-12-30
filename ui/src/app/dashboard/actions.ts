566d83a1dfb66fb983c8f0607a8bd910ad10e6ab
Kevin Chen <kevinmichaelchen@gmail.com>
2023-12-30 09:42:09 -0500

    feat(ui): Toast the Workflow ID

--- ui/src/app/dashboard/actions.ts
+++ ui/src/app/dashboard/actions.ts
@@ -1,24 +1,29 @@
-'use server'
+"use server";
 
-import { Timestamp } from '@bufbuild/protobuf'
-import { createPromiseClient } from '@connectrpc/connect'
-import { TemporalService } from '@buf/kevinmichaelchen_temporalapis.connectrpc_es/temporal/v1beta1/api_connect'
-import { createConnectTransport } from '@connectrpc/connect-web'
+import { Timestamp } from "@bufbuild/protobuf";
+import { createPromiseClient } from "@connectrpc/connect";
+import { TemporalService } from "@buf/kevinmichaelchen_temporalapis.connectrpc_es/temporal/v1beta1/api_connect";
+import { createConnectTransport } from "@connectrpc/connect-web";
 
 const temporalClient = createPromiseClient(
   TemporalService,
   createConnectTransport({
-    baseUrl: 'http://localhost:8081',
+    baseUrl: "http://localhost:8081",
   }),
-)
+);
 
 export async function createOnboardingWorkflow({
   orgName,
   profileName,
   start,
   end,
-}: { orgName: string; profileName: string; start: Date; end: Date }) {
-  'use server'
+}: {
+  orgName: string;
+  profileName: string;
+  start: Date;
+  end: Date;
+}) {
+  "use server";
   const response = await temporalClient.createOnboardingWorkflow({
     org: {
       name: orgName,
@@ -30,5 +35,6 @@ export async function createOnboardingWorkflow({
     profile: {
       name: profileName,
     },
-  })
+  });
+  return response;
 }
