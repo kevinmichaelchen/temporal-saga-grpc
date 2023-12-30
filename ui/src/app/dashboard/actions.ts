a63244cacb93528a3d100823b8e03d9d60435364
Kevin Chen <kevinmichaelchen@gmail.com>
2023-12-29 23:05:22 -0500

    chore: format

--- ui/src/app/dashboard/actions.ts
+++ ui/src/app/dashboard/actions.ts
@@ -1,31 +1,34 @@
 'use server'
 
-import {Timestamp} from "@bufbuild/protobuf";
-import {createPromiseClient} from "@connectrpc/connect";
-import {TemporalService} from "@buf/kevinmichaelchen_temporalapis.connectrpc_es/temporal/v1beta1/api_connect";
-import {createConnectTransport} from "@connectrpc/connect-web";
+import { Timestamp } from '@bufbuild/protobuf'
+import { createPromiseClient } from '@connectrpc/connect'
+import { TemporalService } from '@buf/kevinmichaelchen_temporalapis.connectrpc_es/temporal/v1beta1/api_connect'
+import { createConnectTransport } from '@connectrpc/connect-web'
 
 const temporalClient = createPromiseClient(
   TemporalService,
   createConnectTransport({
-    baseUrl: "http://localhost:8081",
+    baseUrl: 'http://localhost:8081',
   }),
-);
+)
 
 export async function createOnboardingWorkflow({
-                                                 orgName, profileName, start, end
-                                               }: { orgName: string, profileName: string, start: Date, end: Date }) {
-  "use server";
+  orgName,
+  profileName,
+  start,
+  end,
+}: { orgName: string; profileName: string; start: Date; end: Date }) {
+  'use server'
   const response = await temporalClient.createOnboardingWorkflow({
     org: {
-      name: "My Org"
+      name: orgName,
     },
     license: {
       start: Timestamp.fromDate(start),
-      end: Timestamp.fromDate(end)
+      end: Timestamp.fromDate(end),
     },
     profile: {
       name: profileName,
-    }
-  });
+    },
+  })
 }
