d2b2b778e340a45743d2789a7324edefc45ec6fa
Kevin Chen <kevinmichaelchen@gmail.com>
2024-01-14 17:03:08 -0500

    docs: Custom CSS

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -8,6 +8,10 @@ export default defineConfig({
   integrations: [
     starlight({
       title: "temporal-saga-grpc",
+      customCss: [
+        // Relative path to your custom CSS file
+        "./src/styles/custom.css",
+      ],
       social: {
         github: "https://github.com/kevinmichaelchen/temporal-saga-grpc",
       },
