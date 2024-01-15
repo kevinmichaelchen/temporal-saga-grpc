dafe38a0573f99223d56906816cb18f22426fe21
Kevin Chen <kevinmichaelchen@gmail.com>
2024-01-15 18:19:16 -0500

    refactor: Remove redundant package

--- pkg/fxmod/connect/interceptor/interceptor.go
+++ pkg/fxmod/connect/interceptor/interceptor.go
@@ -3,9 +3,9 @@ package interceptor
 
 import (
 	"connectrpc.com/connect"
+	"connectrpc.com/otelconnect"
+	"fmt"
 	"go.uber.org/fx"
-
-	pkgConnect "github.com/kevinmichaelchen/temporal-saga-grpc/pkg/connect"
 )
 
 // Module - An FX module for Connect interceptors.
@@ -17,5 +17,19 @@ var Module = fx.Module("connect/interceptor",
 
 // NewInterceptors - Creates interceptors for Connect Go servers.
 func NewInterceptors() ([]connect.Interceptor, error) {
-	return pkgConnect.Interceptors()
+	interceptor, err := otelconnect.NewInterceptor(
+		// Trust the client's tracing information. With this option, servers
+		// will create child spans for each request.
+		otelconnect.WithTrustRemote(),
+		// Reduce metrics and tracing cardinality. Drop high-cardinality
+		// attributes like the server IP address and the remote port number.
+		otelconnect.WithoutServerPeerAttributes(),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("unable to construct otelconnect interceptor: %w", err)
+	}
+
+	return []connect.Interceptor{
+		interceptor,
+	}, nil
 }
